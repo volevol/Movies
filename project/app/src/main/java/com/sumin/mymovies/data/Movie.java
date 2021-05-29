14c33bed14c481dfa5d4ecc73eb2c88f44ef9bf6
Dimasik <kdpkd@ukr.net>
2021-05-29 19:40:21 +0300

    picasso

--- project/app/src/main/java/com/sumin/mymovies/data/Movie.java
+++ project/app/src/main/java/com/sumin/mymovies/data/Movie.java
@@ -7,20 +7,30 @@ public class Movie {
     private String originalTitle;
     private String overview;
     private String posterPath;
+    private String bigPosterPath;
     private String backdropPath;
     private double voteAverage;
     private String releaseDate;
 
-    public Movie(int id, int voteCount, String title, String originalTitle, String overview, String posterPath, String backdropPath, double voteAverage, String releaseDate) {
+    public Movie(int id, int voteCount, String title, String originalTitle, String overview, String posterPath, String bigPosterPath, String backdropPath, double voteAverage, String releaseDate) {
         this.id = id;
         this.voteCount = voteCount;
         this.title = title;
         this.originalTitle = originalTitle;
         this.overview = overview;
         this.posterPath = posterPath;
+        this.bigPosterPath = bigPosterPath;
         this.backdropPath = backdropPath;
         this.voteAverage = voteAverage;
         this.releaseDate = releaseDate;
+    }
+
+    public String getBigPosterPath() {
+        return bigPosterPath;
+    }
+
+    public void setBigPosterPath(String bigPosterPath) {
+        this.bigPosterPath = bigPosterPath;
     }
 
     public int getId() {
