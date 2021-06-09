5eedaff78388f03309e61f01b23727f3a2c5ee52
DNebo <richardfannin1919@gmail.com>
2021-06-09 14:54:54 +0300

    commit

--- project/app/src/main/java/com/sumin/mymovies/DetailActivity.java
+++ project/app/src/main/java/com/sumin/mymovies/DetailActivity.java
@@ -1,13 +1,80 @@
 package com.sumin.mymovies;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.sumin.mymovies.data.FavouriteMovie;
+import com.sumin.mymovies.data.MainViewModel;
+import com.sumin.mymovies.data.Movie;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private ImageView imageViewAddToFavourite;
+    private ImageView imageViewBigPoster;
+    private TextView textViewTitle;
+    private TextView textViewOriginalTitle;
+    private TextView textViewRating;
+    private TextView textViewReleaseDate;
+    private TextView textViewOverview;
+
+    private int id;
+    private Movie movie;
+    private FavouriteMovie favouriteMovie;
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        imageViewBigPoster = findViewById(R.id.imageViewBigPoster);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewOriginalTitle = findViewById(R.id.textViewOriginalTitle);
+        textViewRating = findViewById(R.id.textViewRating);
+        textViewReleaseDate = findViewById(R.id.textViewReleaseDate);
+        textViewOverview = findViewById(R.id.textViewOverview);
+        imageViewAddToFavourite = findViewById(R.id.imageViewAddToFavourite);
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("id")) {
+            id = intent.getIntExtra("id", -1);
+        } else {
+            finish();
+        }
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        movie = viewModel.getMovieById(id);
+        Picasso.get().load(movie.getBigPosterPath()).into(imageViewBigPoster);
+        textViewTitle.setText(movie.getTitle());
+        textViewOriginalTitle.setText(movie.getOriginalTitle());
+        textViewOverview.setText(movie.getOverview());
+        textViewReleaseDate.setText(movie.getReleaseDate());
+        textViewRating.setText(Double.toString(movie.getVoteAverage()));
+        setFavourite();
+    }
+
+    public void onClickChangeFavourite(View view) {
+        if (favouriteMovie == null) {
+            viewModel.insertFavouriteMovie(new FavouriteMovie(movie));
+            Toast.makeText(this, R.string.add_to_favourite, Toast.LENGTH_SHORT).show();
+        } else {
+            viewModel.deleteFavouriteMovie(favouriteMovie);
+            Toast.makeText(this, R.string.remove_from_favourite, Toast.LENGTH_SHORT).show();
+        }
+        setFavourite();
+    }
+
+    private void setFavourite() {
+        favouriteMovie = viewModel.getFavouriteMovieById(id);
+        if (favouriteMovie == null) {
+            imageViewAddToFavourite.setImageResource(R.drawable.favourite_add_to);
+        } else {
+            imageViewAddToFavourite.setImageResource(R.drawable.favourite_remove);
+        }
     }
 }
--- project/app/src/main/java/com/sumin/mymovies/data/MainViewModel.java
+++ project/app/src/main/java/com/sumin/mymovies/data/MainViewModel.java
@@ -13,11 +13,13 @@ public class MainViewModel extends AndroidViewModel {
 
     private static MovieDatabase database;
     private LiveData<List<Movie>> movies;
+    private LiveData<List<FavouriteMovie>> favouriteMovies;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
         database = MovieDatabase.getInstance(getApplication());
         movies = database.movieDao().getAllMovies();
+        favouriteMovies = database.movieDao().getAllFavouriteMovies();
     }
 
     public Movie getMovieById(int id) {
@@ -29,6 +31,21 @@ public class MainViewModel extends AndroidViewModel {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public FavouriteMovie getFavouriteMovieById(int id) {
+        try {
+            return new GetFavouriteMovieTask().execute(id).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public LiveData<List<FavouriteMovie>> getFavouriteMovies() {
+        return favouriteMovies;
     }
 
     public void deleteAllMovies() {
@@ -45,6 +62,34 @@ public class MainViewModel extends AndroidViewModel {
 
     public LiveData<List<Movie>> getMovies() {
         return movies;
+    }
+
+    public void insertFavouriteMovie(FavouriteMovie movie) {
+        new InsertFavouriteTask().execute(movie);
+    }
+
+    public void deleteFavouriteMovie(FavouriteMovie movie) {
+        new DeleteFavouriteTask().execute(movie);
+    }
+
+    private static class DeleteFavouriteTask extends AsyncTask<FavouriteMovie, Void, Void> {
+        @Override
+        protected Void doInBackground(FavouriteMovie... movies) {
+            if (movies != null && movies.length > 0) {
+                database.movieDao().deleteFavouriteMovie(movies[0]);
+            }
+            return null;
+        }
+    }
+
+    private static class InsertFavouriteTask extends AsyncTask<FavouriteMovie, Void, Void> {
+        @Override
+        protected Void doInBackground(FavouriteMovie... movies) {
+            if (movies != null && movies.length > 0) {
+                database.movieDao().insertFavouriteMovie(movies[0]);
+            }
+            return null;
+        }
     }
 
     private static class DeleteTask extends AsyncTask<Movie, Void, Void> {
@@ -80,6 +125,16 @@ public class MainViewModel extends AndroidViewModel {
         protected Movie doInBackground(Integer... integers) {
             if (integers != null && integers.length > 0) {
                 return database.movieDao().getMovieById(integers[0]);
+            }
+            return null;
+        }
+    }
+
+    private static class GetFavouriteMovieTask extends AsyncTask<Integer, Void, FavouriteMovie> {
+        @Override
+        protected FavouriteMovie doInBackground(Integer... integers) {
+            if (integers != null && integers.length > 0) {
+                return database.movieDao().getFavouriteMovieById(integers[0]);
             }
             return null;
         }
